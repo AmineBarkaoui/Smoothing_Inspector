64364bbdf53efe2a9cdbab5050830a563528c723
AmineBarkaoui <amine.barkaoui1@gmail.com>
2022-07-07 17:49:12 +0200

    Update garcia and remove some params

--- smoothing.py
+++ smoothing.py
@@ -1,20 +1,6 @@
-#import calendar
-#from datetime import date
-#import datetime
-from math import log10
-#import os
-#import re
-#import time
-
-#import glob
-#import matplotlib.pyplot as plt
 import numpy as np
-import rasterio as rio
-
-import rioxarray as rx
-import pandas as pd
-#from tqdm import tqdm
 import xarray as xr
+from math import log10
 
 # V_curve and WCV smoothing module
 import seasmon_xr
@@ -28,23 +14,23 @@ def smooth(da, vcurve, garcia, wcv, robust, p_v, p_wcv, srange=None):
     ds = da.to_dataset(name='band')
     nodata = -3000.
     
+    
     if srange==None:
         srange = np.arange(-2, 4.2, 0.2, dtype=np.float64)
     
     if vcurve:
-        if p_vc != None:
-            ds_smoothed_v = da.hdc.whit.whitsvc(nodata = nodata, srange = srange, p = p)
+        if p_v != None:
+            ds_smoothed_v = da.hdc.whit.whitsvc(nodata = nodata, srange = srange, p = p_v)
         else:
             ds_smoothed_v = da.hdc.whit.whitsvc(nodata = nodata, srange = srange)            
-
+        
         ds['smoothed_v'] = ds_smoothed_v.band
         ds['Sopts_v'] = ds_smoothed_v.sgrid
-        ds['fits_v'] = ds_smoothed_v.fits
-        ds['pens_v'] = ds_smoothed_v.pens
+        ds['curv'] = ds_smoothed_v.curv
     
     if wcv:
-        if p_bool:
-            ds_smoothed_wcv = da.hdc.whit.whitsgcv(nodata = nodata, srange = srange, p = p, robust = robust)
+        if p_wcv != None:
+            ds_smoothed_wcv = da.hdc.whit.whitsgcv(nodata = nodata, srange = srange, p = p_wcv, robust = robust)
         else:
             ds_smoothed_wcv = da.hdc.whit.whitsgcv(nodata = nodata, srange = srange, robust = robust)
 
@@ -53,43 +39,19 @@ def smooth(da, vcurve, garcia, wcv, robust, p_v, p_wcv, srange=None):
         
     if garcia:
         # Reformat the Data
-        #z = da.transpose('latitude', 'longitude', 'time')
-        z = z.to_dataset(name='band')
+        z = da.to_dataset(name='band')
         
         # Clean the dataset
         z['band_n'] = replace_bad(z.band)
-        
-        # Masking out pixels
-        z['nan_mask'] = nan_mask(z.band_n)   
-        
-            
-        def Garcia_wrapper(x, mask):
-            if not mask:
-                # If we want to skip, return -3000 for all timeseries values
-                # and the S_opt
-                return ((-3000) * np.ones(x.shape[0]), -3000)
-            else:
-                # Doing the actual smoothing
-                g_smooth_rens = Garcia_smoothing_complete(x,
-                                                          fit_robust=robust,
-                                                          fit_envelope=(p_wcv!=None),
-                                                          neg_residuals_only=True,
-                                                          Sopt_Rog=True,
-                                                          Sopt_range=srange)
-                smoothed_ndvi = g_smooth_rens[0]
-                Sopt = g_smooth_rens[1][-1,1]
-                return (smoothed_ndvi, Sopt)
   
-        Sopts_g = xr.apply_ufunc(Garcia_wrapper,
-                                     z.band_n[:, :, :].astype(np.float32),
-                                     z.nan_mask[:, :],
-                                     input_core_dims=[['time'],[]],
-                                     output_core_dims=[['time'], []],
-                                     vectorize = True,
-                                     dask = 'parallelized',
-                                     output_dtypes=[np.float32, np.float32])
+        g_smooth_rens = Garcia_smoothing_complete(z.band_n.values.astype(np.float32),
+                                                  fit_robust=robust,
+                                                  fit_envelope=(p_wcv!=None),
+                                                  neg_residuals_only=True,
+                                                  Sopt_Rog=True,
+                                                  Sopt_range=srange)
+        ds['smoothed_g'] = np.int16(g_smooth_rens[0])
+        ds['Sopts_g'] = np.int16(g_smooth_rens[1][-1,1])
         
-        ds['smoothed_g'] = Sopts_g[0]
-        ds['Sopts_g'] = log10(Sopts_g[1])
         
     return ds
