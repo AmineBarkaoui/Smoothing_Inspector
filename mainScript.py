f1d98446c59b3bb04c6274ed67db50fae6cd62b2
AmineBarkaoui <amine.barkaoui1@gmail.com>
2022-07-07 17:48:53 +0200

    Add plot_vcurve

--- mainScript.py
+++ mainScript.py
@@ -4,7 +4,7 @@ import time
 import numpy as np
 import xarray as xr
 import pandas as pd
-#import altair as alt
+import altair as alt
 import streamlit as st
 
 from read_data import read_data
@@ -12,37 +12,65 @@ from smoothing import smooth
 
     
 
-def main_plot(smoothed, methods, col2):
-
+def plot_main(smoothed, methods, col2):
     
-#    if sr != None:
-#    	srange = np.arange(sr[0], sr[1], 0.2)
-#    	srange_bool = True
-#    	smoothed = smooth(da, methods['raw'], methods['garcia'], methods['vcurve'], methods['wcv'], 
-#    					  srange_bool, robust, pval_wcv, pval_vc, srange)
-#    else:
-#    	srange_bool = False 
-#    	smoothed = smooth(da, methods['raw'], methods['garcia'], methods['vcurve'], methods['wcv'], 
-#    					  srange_bool, robust, pval_wcv, pval_vc)
-
-    # # FOR NOW
-    # smoothed = da.to_dataset(name = 'raw')
-    # smoothed['vcurve'] = da
-    # smoothed['garcia'] = da
-    # smoothed['wcv'] = da
-    # ######################
-
-    # ORDER: Raw, vcurve, garcia, wcv
-    names = ['band', 'smoothed_v', 'smoothed_g', 'smoothed_wcv']
-    dfp = pd.DataFrame(index = smoothed.Date.values)      
+    # Create DataFrame
+    names = ['smoothed_v', 'smoothed_g', 'smoothed_wcv']
+    df = pd.DataFrame(index = smoothed.time.values) 
+    df.index.name = 'time'     
     for i,sm in enumerate(methods.keys()):   
         if methods[sm]:
-            dfp[sm] = smoothed[names[i]]
+            df[sm] = smoothed[names[i]]
+    df = df.reset_index()
+    df = df.melt('time', var_name='name', value_name='value')
+    
+    dfraw = pd.DataFrame(index = smoothed.time.values) 
+    dfraw['raw'] = smoothed['band']
+    dfraw.index.name = 'time'
+    dfraw = dfraw.reset_index()
+    dfraw = dfraw.melt('time', var_name='name', value_name='value')
+    
+    valid = list(methods.values())
+    names = np.array(['vcurve', 'garcia', 'wcv'])[valid].tolist()
+    colors = np.array(['red', 'blue', 'green'])[valid].tolist()
+    
+    chart1 = alt.Chart(df).mark_line(opacity=0.6, size = 1).encode(
+      x=alt.X('time'),
+      y=alt.Y('value'),
+      color=alt.Color('name', scale=alt.Scale(
+            domain=names,
+            range=colors))
+      ).properties(title="Smoothing")
+      
+    chart2 = alt.Chart(dfraw).mark_line(color = 'grey', opacity=0.6, size = 1, point=alt.OverlayMarkDef(opacity = 0.6, size = 10)).encode(
+      x=alt.X('time'),
+      y=alt.Y('value')
+      ).properties(title="Smoothing")
+    
+    layers = alt.layer(chart1, chart2).configure_area(tooltip = True).interactive()
+    
+    col2.altair_chart(layers, use_container_width=True)
+    
 
+def plot_vcurve(smoothed, methods, col2):
+    
+    # Create DataFrame
+    print(smoothed['curv'][0])
+    df = pd.DataFrame(index = smoothed['curv'][0]) 
+    df.index.name = 'x'     
 
-    col2.line_chart(dfp)
+    df['curv'] = smoothed['curv'][1]
+    df = df.reset_index()
+    df = df.melt('x', var_name='name', value_name='value')
+    
+    # Utils
 
-
+    chart = alt.Chart(df).mark_line(opacity=0.6).encode(
+      x=alt.X('x'),
+      y=alt.Y('value')
+      ).properties(title="V curve")
+    
+    col2.altair_chart(chart, use_container_width=True)
 
 
 @st.cache  # No need for TTL this time. It's static data :)
@@ -87,12 +115,11 @@ def main():
     
     col1.markdown('------------')
     
-    raw = True #col1.checkbox('Raw',value=True)
     vcurve = col1.checkbox('V-curve',value=True)
-    garcia = col1.checkbox('Garcia',value=False)
+    garcia = col1.checkbox('Garcia',value=True)
     wcv = col1.checkbox('WCV',value=True)
-    methods = dict(raw = raw, vcurve = vcurve, garcia = garcia, wcv = wcv)
-        
+    methods = dict(vcurve = vcurve, garcia = garcia, wcv = wcv)       
+    
     col1.markdown('------------')
     
     bound = col1.checkbox('Set bounds to Sopt',value=False)
@@ -107,8 +134,8 @@ def main():
     
     col1.markdown('------------')
     
-    expec_wcv = col1.checkbox('Set a p value - WCV',value=True)
-    if expec_wcv:
+    expec = col1.checkbox('Set a p value',value=True)
+    if expec:
         pval_wcv = col1.select_slider('Select a p value for the WCV',
                                 options=[0., 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.],
                                 value = 0.8)
@@ -127,18 +154,24 @@ def main():
     
     df = ndvi_MOD.loc[loc]
     
-    da = xr.DataArray(np.array(df['NDVI']), dims = ['time'], coords = dict(time = df['Date']))
-        
+    da = xr.DataArray(np.array(df['NDVI'])*10000, dims = ['time'], coords = dict(time = df['Date']))
+
     smoothed = smooth(da, vcurve, garcia, wcv, robust, pval_wcv, pval_vc, sr)
 
-    print(smoothed)
     print("--- %s seconds SMOOTH---" % (time.time() - start_time))
     
 # =============================================================================
 #   Main plot
 # =============================================================================
     
-    main_plot(smoothed, methods, col2)
+    plot_main(smoothed, methods, col2)
+    
+# =============================================================================
+#   V-curve plot
+# =============================================================================
+    
+    if vcurve:
+        plot_vcurve(smoothed, methods, col2)
 
 
 
