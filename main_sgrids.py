f055ac6c4047101d057ae7afe9954fe240cad961
AmineBarkaoui <66622330+AmineBarkaoui@users.noreply.github.com>
2023-05-22 09:30:05 +0200

    Transform altair plots into plt for memory reasons

--- main_sgrids.py
+++ main_sgrids.py
@@ -3,6 +3,8 @@ import xarray as xr
 import altair as alt
 import pandas as pd
 import streamlit as st
+from itertools import product
+import matplotlib.pyplot as plt
 
 from streamlit_option_menu import option_menu
 
@@ -10,7 +12,7 @@ METHODS = ['pvc_unconstrained','pvc_constrained','pwcv_basic','pwcv_robust']
 
 TITLES = dict(zip(METHODS, ['Unconstrained V-curve', 'Constrained V-curve', 'Basic WCV', 'Robust WCV']))
 
-def plot_main(df):
+def plot_main_altair(df):
     
     df = df.drop('spatial_ref', axis=1).reset_index()
     df = df.melt(id_vars=['longitude','latitude'])
@@ -32,6 +34,25 @@ def plot_main(df):
     )
 
     st.altair_chart(chart, use_container_width=True)
+
+
+def plot_main_plt(tile, index):
+
+    ds = read_data(tile, index) 
+
+    fig, axs = plt.subplots(2, 2, sharex=True, sharey=True)
+
+    for i,j in product(range(2), range(2)):
+        im = ds[METHODS[2 * i + j]].plot.imshow(ax=axs[i,j], vmin=-4, vmax=4, add_colorbar=False,)
+        axs[i,j].set_title(TITLES[METHODS[2 * i + j]])
+        axs[i,j].set_xlabel('', fontsize=0)
+        axs[i,j].set_ylabel('', fontsize=0)
+
+    fig.subplots_adjust(right=0.875) #also try using kwargs bottom, top, or hspace 
+    cbar_ax = fig.add_axes([0.1, -0.1, .8, .05]) #left, bottom, width, height
+    fig.colorbar(im, cax=cbar_ax, orientation="horizontal")
+
+    st.pyplot(fig, use_container_width=True)
     
 
 def read_data(tile, ind):
@@ -44,15 +65,17 @@ def read_data(tile, ind):
         return None
     
     
-@st.cache_resource  # No need for TTL this time. It's static data :)
+@st.cache_data # _resource  # No need for TTL this time. It's static data :)
 def get_data_by_state():
     tiles = glob.glob('data/sgrids_ndvi/pwcv_basic/*')
     tiles = sorted([p.split('\\')[-1] for p in tiles])
 
     ndvi_tiles = dict(zip(tiles, [read_data(tile, 'ndvi') for tile in tiles]))
-    tda_tiles = dict(zip(tiles, [read_data(tile, 'tda') for tile in tiles]))
-    tna_tiles = dict(zip(tiles, [read_data(tile, 'tna') for tile in tiles]))
-    return ndvi_tiles, tda_tiles, tna_tiles
+    #tda_tiles = dict(zip(tiles, [read_data(tile, 'tda') for tile in tiles]))
+    #tna_tiles = dict(zip(tiles, [read_data(tile, 'tna') for tile in tiles]))
+
+    tiles = list(ndvi_tiles.keys())
+    return tiles #, tda_tiles, tna_tiles
 
 
 def main():
@@ -69,9 +92,9 @@ def main():
 #    Data 
 # =============================================================================
         
-    ndvi_tiles, tda_tiles, tna_tiles = get_data_by_state()
+    tiles = get_data_by_state()
         
-    tiles = list(ndvi_tiles.keys())
+    
     
 # =============================================================================
 #   Widgets inputs  
@@ -92,15 +115,15 @@ def main():
 #   Selection of dataset
 # =============================================================================
     
-    all_ds_indexes = dict(NDVI = ndvi_tiles, TDA = tda_tiles, TNA = tna_tiles)
+    #all_ds_indexes = dict(NDVI = ndvi_tiles, TDA = tda_tiles, TNA = tna_tiles)
     
-    ds_to_plot = all_ds_indexes[index][tile]
+    #ds_to_plot = all_ds_indexes[index][tile]
     
 # =============================================================================
 #   Main plot
 # =============================================================================
     
-    plot_main(ds_to_plot.to_dataframe())
+    plot_main_plt(tile, index) #.to_dataframe())
     
 
 if __name__ == "__main__":
